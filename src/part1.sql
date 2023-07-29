6483072ce583db11346591d27f6040c8fda0b7e4
Nyarlathotep Greyfeather <nyarlath@ge-d4.kzn.21-school.ru>
2023-07-29 14:34:45 +0300

    add insert 5 record

--- src/part1.sql
+++ src/part1.sql
@@ -87,3 +87,93 @@ CREATE TABLE TimeTracking (
   State INT,
   FOREIGN KEY (PeerNickname) REFERENCES Peers(PeerNickname)
 );
+
+-- Заполнение таблицы Peers
+INSERT INTO Peers (PeerNickname, Birthday)
+VALUES
+  ('peer1', '1990-01-01'),
+  ('peer2', '1992-05-10'),
+  ('peer3', '1988-12-15'),
+  ('peer4', '1995-03-22'),
+  ('peer5', '1991-09-07');
+
+-- Заполнение таблицы Tasks
+INSERT INTO Tasks (TaskName, EntryTaskName, MaxXP)
+VALUES
+  ('task1', NULL, 100),
+  ('task2', 'task1', 200),
+  ('task3', 'task1', 150),
+  ('task4', 'task2', 300),
+  ('task5', 'task3', 250);
+
+-- Заполнение таблицы P2P
+INSERT INTO P2P (ID, CheckID, PeerNickname, P2PStatus, Time)
+VALUES
+  (1, 1, 'peer1', 'Start', '2023-07-01 10:00:00'),
+  (2, 1, 'peer2', 'Success', '2023-07-01 11:00:00'),
+  (3, 2, 'peer3', 'Start', '2023-07-02 09:00:00'),
+  (4, 2, 'peer4', 'Failure', '2023-07-02 10:30:00'),
+  (5, 3, 'peer5', 'Start', '2023-07-03 14:00:00');
+
+-- Заполнение таблицы Verter
+INSERT INTO Verter (ID, CheckID, VerterStatus, Time)
+VALUES
+  (1, 1, 'Start', '2023-07-01 11:30:00'),
+  (2, 1, 'Success', '2023-07-01 12:00:00'),
+  (3, 2, 'Start', '2023-07-02 11:00:00'),
+  (4, 2, 'Failure', '2023-07-02 12:30:00'),
+  (5, 3, 'Start', '2023-07-03 15:00:00');
+
+-- Заполнение таблицы Checks
+INSERT INTO Checks (ID, PeerNickname, TaskName, CheckDate)
+VALUES
+  (1, 'peer1', 'task1', '2023-07-01'),
+  (2, 'peer2', 'task2', '2023-07-02'),
+  (3, 'peer3', 'task3', '2023-07-03'),
+  (4, 'peer4', 'task4', '2023-07-04'),
+  (5, 'peer5', 'task5', '2023-07-05');
+
+-- Заполнение таблицы TransferredPoints
+INSERT INTO TransferredPoints (ID, ReviewerPeerNickname, ReviewedPeerNickname, TotalPoints)
+VALUES
+  (1, 'peer1', 'peer2', 1),
+  (2, 'peer1', 'peer3', 2),
+  (3, 'peer2', 'peer4', 3),
+  (4, 'peer3', 'peer5', 4),
+  (5, 'peer4', 'peer5', 2);
+
+-- Заполнение таблицы Friends
+INSERT INTO Friends (ID, Peer1Nickname, Peer2Nickname)
+VALUES
+  (1, 'peer1', 'peer2'),
+  (2, 'peer2', 'peer3'),
+  (3, 'peer3', 'peer4'),
+  (4, 'peer4', 'peer5'),
+  (5, 'peer5', 'peer1');
+
+-- Заполнение таблицы Recommendations
+INSERT INTO Recommendations (ID, PeerNickname, RecommendedPeerNickname)
+VALUES
+  (1, 'peer1', 'peer3'),
+  (2, 'peer2', 'peer4'),
+  (3, 'peer3', 'peer5'),
+  (4, 'peer4', 'peer1'),
+  (5, 'peer5', 'peer2');
+
+-- Заполнение таблицы XP
+INSERT INTO XP (ID, CheckID, EarnedXP)
+VALUES
+  (1, 1, 50),
+  (2, 2, 100),
+  (3, 3, 75),
+  (4, 4, 150),
+  (5, 5, 125);
+
+-- Заполнение таблицы TimeTracking
+INSERT INTO TimeTracking (ID, PeerNickname, Date, Time, State)
+VALUES
+  (1, 'peer1', '2023-07-01', '08:00:00', 1),
+  (2, 'peer2', '2023-07-01', '09:00:00', 1),
+  (3, 'peer3', '2023-07-01', '10:00:00', 1),
+  (4, 'peer4', '2023-07-01', '11:00:00', 1),
+  (5, 'peer5', '2023-07-01', '12:00:00', 1);
